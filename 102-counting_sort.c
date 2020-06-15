084c9e49132f4982ae90624ebb6d1e04854fa30f
santiagopemo <santiagopemo@gmail.com>
2020-06-15 21:05:25 +0000

    added task 102

---
+++ 102-counting_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+ * counting_sort - function that sorts an array of integers
+ * in ascending order using the Counting sort algorithm
+ * @array: array to sort
+ * @size: size of the array to sort
+ */
+void counting_sort(int *array, size_t size)
+{
+	int *output, *count, max, i, o_idx, c_idx;
+
+	if (size < 2)
+		return;
+	output = malloc(sizeof(int) * size);
+	if (output == NULL)
+		return;
+	max = array[0];
+	for (i = 0; i < (int)size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	count = malloc(sizeof(int) * max + 1);
+	if (count == NULL)
+	{
+		free(output);
+		return;
+	}
+	for (i = 0; i <= max; i++)
+		count[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		count[array[i]]++;
+	for (i = 1; i <= max; i++)
+		count[i] += count[i - 1];
+	for (i = 0; i < (int)size; i++)
+	{
+		c_idx = array[i];
+		o_idx = count[c_idx] - 1;
+		output[o_idx] = array[i];
+		count[c_idx] -= 1;
+	}
+	for (i = 0; i < (int)size; i++)
+		array[i] = output[i];
+	print_array(count, max + 1);
+	free(output);
+	free(count);
+}
