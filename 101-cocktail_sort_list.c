7e86658808ec392123e7c639a7adff843a33aa8d
santiagopemo <santiagopemo@gmail.com>
2020-06-15 18:49:21 +0000

    corrected loop

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -25,17 +25,15 @@ void swap(listint_t *a, listint_t *b, listint_t **list)
  */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *start, *end, *i;
+	listint_t *i;
 	int swapped = 1;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	for (end = *list; end->next; end = end->next)
-		continue;
-	for (start = *list; swapped; start = start->next)
+	for (; swapped;)
 	{
 		swapped = 0;
-		for (i = start; i->next && i != end;)
+		for (i = *list; i->next;)
 		{
 			if (i->n > i->next->n)
 			{
@@ -49,8 +47,7 @@ void cocktail_sort_list(listint_t **list)
 		if (swapped == 0)
 			break;
 		swapped = 0;
-		end = end->prev;
-		for (i = end->prev; i && i != start->prev;)
+		for (i = i->prev; i->prev; i = i->prev)
 		{
 			if (i->n > i->next->n)
 			{
@@ -58,8 +55,6 @@ void cocktail_sort_list(listint_t **list)
 				print_list(*list);
 				swapped = 1;
 			}
-			else
-				i = i->prev;
 		}
 	}
 }
